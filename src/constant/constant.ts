e2f8af057db3974fae235c8a97aef9b6bb6cded6
ThuanLeMerkle <Thuan.Le@merkle.com>
2024-01-08 20:57:05 +0700

    init project

---
+++ src/constant/constant.ts
@@ -0,0 +1,4 @@
+export const enum SCREENS {
+    SPLASHSCREEN = 'SPLASHSCREEN',
+    HOME = 'HOME'
+}
---
+++ src/navigations/TypeCheck.ts
@@ -0,0 +1,11 @@
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {SCREENS} from '../constant/constant';
+import {RouteProp} from '@react-navigation/native';
+
+type MainStackParams = {
+  HOME: undefined;
+  SPLASHSCREEN: undefined;
+};
+
+export type PropsPush = NativeStackScreenProps<MainStackParams, SCREENS.HOME>;
+export type PropsRoute = RouteProp<MainStackParams, SCREENS.HOME>;
